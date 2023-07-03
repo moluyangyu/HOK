a3a50ee63d899983b9ba0cb836e4f371c0a27db6
moluyangyu <985286227@qq.com>
2023-07-03 10:05:31 +0800

    训练集

--- _爬虫/批量获取王者荣耀皮肤.py
+++ _爬虫/批量获取王者荣耀皮肤.py
@@ -1,9 +1,11 @@
 #1.发送请求
 #发送请求的模块（为第三方库，需要在外部安装） pip install requests
+
 import requests 
 #解析html代码的工具
 from lxml import etree
 import os
+import numpy as np
 from time import sleep
 
 #伪装自己
@@ -12,10 +14,11 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 hero_list_url = 'https://pvp.qq.com/web201605/js/herolist.json'
 hero_list_resp = requests.get(hero_list_url,headers=headers)
 print(hero_list_resp.json())
+cnames=[]
 for h in hero_list_resp.json():
     ename = h.get('ename')
     cname = h.get('cname')
-
+    cnames.append(cname)
     if not os.path.exists(cname):
         os.makedirs(cname)
     hero_info_url = f'https://pvp.qq.com/web201605/herodetail/{ename}.shtml'
@@ -29,11 +32,13 @@ for h in hero_list_resp.json():
 
     #发送请求
     for i,n in enumerate(names):
-        resp = requests.get(f'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/{ename}/{ename}-bigskin-{i+1}.jpg')
+        #resp = requests.get(f'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/{ename}/{ename}-bigskin-{i+1}.jpg')
         #接收服务器响应的图片（皮肤）
         #保存图片（皮肤）
-        with open(f'train/{n}.jpg','wb') as f:
-            f.write(resp.content)
+        #with open(f'train/{n}.jpg','wb') as f:
+            #f.write(resp.content)
         print(f'已下载:{n}的皮肤')
-        sleep(1)
+        #sleep(1)
+    print(cnames)
+    print(np.size(cnames))
 
