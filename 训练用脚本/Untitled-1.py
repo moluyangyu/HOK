af40466c7a97e1c203f9538e3ff003bea1560a4d
syh-77 <3365334923@qq.com>
2023-07-06 09:00:36 +0800

    对视频进行切片，对多个视频循环切片，更新切片频率

--- 训练用脚本/Untitled-1.py
+++ 训练用脚本/Untitled-1.py
@@ -1,33 +1,42 @@
 import cv2
+import os
 
-video_path = 'C:/Users/86135/Desktop/program/new/pythonAi/西游记10.mp4'
-output_folder = 'C:/Users/86135/Desktop/program/new/pythonAi/picture/'
+video_folder = "E:/11111111/film(2)/film"  # 替换为包含多个视频文件的文件夹路径
+output_folder = "E:/11111111/111/"  # 替换为你希望保存图像文件的实际输出文件夹路径
 
-# 打开视频文件
-video_capture = cv2.VideoCapture(video_path)
+# 创建输出文件夹
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-# 检查视频是否成功打开
-if not video_capture.isOpened():
-    print("无法打开视频文件")
-    exit()
+# 遍历视频文件夹中的每个视频文件
+for video_file in os.listdir(video_folder):
+    video_path = os.path.join(video_folder, video_file)
 
-# 逐帧读取视频并保存为图像文件
-frame_count = 0
-while True:
-    success, frame = video_capture.read()
+    cap = cv2.VideoCapture(video_path)
 
-    # 检查是否成功读取帧
-    if not success:
-        break
+    if not cap.isOpened():
+        print("Failed to open video file:", video_file)
+        continue
 
-    # 保存图像文件
-    output_file = output_folder + 'frame_{:04d}.jpg'.format(frame_count)
-    cv2.imwrite(output_file, frame)
+    # 定义每隔多少帧保存一张图像文件
+    save_frequency = 50
+    frame_count = 0
 
-    frame_count += 1
+    # 读取视频帧
+    while True:
+        ret, frame = cap.read()
 
-# 释放视频资源
-video_capture.release()
-#python train.py --img 768*576 --batch 16 --epochs 50 --data C:/Users/86135/Desktop/program/new/pythonAi/data.yaml --cfg C:/Users/86135/AppData/Local/Programs/Python/Python310/yolov5/models/yolov5m.yaml  --name C:/Users/86135/Desktop/program/new/pythonAi/yolov5s_tang_seng
-#python train.py --device 0 --img-size 768 --batch-size 21 --epochs 100 --data C:/Users/86135/Desktop/program/new/pythonAi/data.yaml --cfg C:/Users/86135/AppData/Local/Programs/Python/Python310/yolov5/models/yolov5s.yaml --weights '' --name C:/Users/86135/Desktop/program/new/pythonAi/yolov5s_train
-#python val.py --data C:/Users/86135/Desktop/program/new/pythonAi/data.yaml --weights C:/Users/86135/Desktop/program/new/pythonAi/yolov5s_train2/weights/best.pt
+        if not ret:
+            break
+
+        # 保存图像文件
+        if frame_count % save_frequency == 0:
+            output_file = os.path.join(output_folder, os.path.splitext(video_file)[0], 'frame_{:04d}.jpg'.format(frame_count))
+            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+            cv2.imwrite(output_file, frame)
+
+        frame_count += 1
+
+    cap.release()
+
+cv2.destroyAllWindows()
